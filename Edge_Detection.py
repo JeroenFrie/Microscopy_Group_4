c78a22cd55be5cba0c0a42381ada02d164483ff2
JeroenFrie <j.a.w.frieling@student.tue.nl>
2023-12-13 17:24:38 +0300

    Beginnende code voor edge detection en closing

--- Edge_Detection.py
+++ Edge_Detection.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 path = "C:/Users/20202619/OneDrive - TU Eindhoven/Vakken/CBL Microscopy/Python_Code_Microscope_Redux/test_1_Con_3.png"
+save_path = "C:/Users/20202619/OneDrive - TU Eindhoven/Vakken/CBL Microscopy/Python_Code_Microscope_Redux/test_1_Con_3_edge.png"
 img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
 
 img_blur = cv2.GaussianBlur(img, (3,3), 0)
@@ -14,18 +15,19 @@ plt.hist(sobelxy.ravel(),256,[0,256])
 plt.title('Histogram for gray scale image')
 plt.show()
 
-kernel_begin = np.ones((9,9),np.uint8)
+kernel_begin = np.ones((8,8),np.uint8)
 #dilation = cv2.dilate(sobelxy,kernel,iterations = 1)
 closing_1 = cv2.morphologyEx(sobelxy, cv2.MORPH_CLOSE, kernel_begin)
 
-kernel_middle = np.ones((7,7),np.uint8)
+kernel_middle = np.ones((2,2),np.uint8)
 closing_2 = cv2.morphologyEx(closing_1, cv2.MORPH_CLOSE, kernel_middle)
 
-kernel_end = np.ones((10,10),np.uint8)
-closing_3 = cv2.morphologyEx(closing_1, cv2.MORPH_CLOSE, kernel_end)
+kernel_end = np.ones((2,2),np.uint8)
+closing_3 = cv2.morphologyEx(closing_2, cv2.MORPH_CLOSE, kernel_end)
 
 
 while(True):
     cv2.imshow('Test', closing_3)
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imwrite(save_path, closing_3)
         break
