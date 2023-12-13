fdd0abbe2fe4e55ac52d91dbe143987dd40bf327
JeroenFrie <j.a.w.frieling@student.tue.nl>
2023-12-13 14:59:31 +0300

    Fixed naming of files

--- Cam_Code.py
+++ Cam_Code.py
@@ -1,10 +1,16 @@
 import cv2
+import random
 
 # define a video capture object
 vid = cv2.VideoCapture(0)
-
+Image_Name = ""
+for i in range(10):
+    Image_Name = Image_Name + str(random.randint(1,100))
+Image_Name = "Image_"+Image_Name+".png"
+#Change depending on if you're not using my (Jeroen) laptop
+File_Pathing = "C:/Users/20202619/OneDrive - TU Eindhoven (1)/Documents/TUe/Vakken/test/"
 #Filepath where image is written
-file_path = "C:/Users/20202619/OneDrive - TU Eindhoven (1)/Documents/TUe/Vakken/test/test.png"
+file_path = File_Pathing + Image_Name
 
 while(True):
 
